214fc8b309f32ec6af9d6b06a5c55c4c5fbf0216
Jester-2-6 <cwimalasuriya@gmail.com>
2019-06-01 18:37:33 +0530

    slave modified

--- src/Main_codes/slave.v
+++ src/Main_codes/slave.v
@@ -1,15 +1,22 @@
+/*
+Module name  : slave.v
+Author 	     : C.Wimalasuriya
+Date Modified: 01/06/2019
+Organization : ABruTECH
+Description  : Slave module of the bus
+*/
 module slave #(
-    parameter mem_offset = 0,
-    parameter mem_size = 2048,
-    parameter addr_width = 12,
-    parameter data_width = 8
+    parameter MEM_OFFSET = 0,
+    parameter MEM_SIZE = 2048,
+    parameter ADDRESS_WIDTH = 12,
+    parameter DATA_WIDTH = 8
 )(
-    input [addr_width - 1:0] addr_in,
+    input [ADDRESS_WIDTH - 1:0] addr_in,
     input clk, rstn, rw_select,
-    input [data_width - 1:0] data_in_parellel,
+    input [DATA_WIDTH - 1:0] data_in_parellel,
 
-    output ready, done,
-    output [data_width - 1:0] data_out_parellel,
+    output reg ready, done,
+    output reg [DATA_WIDTH - 1:0] data_out_parellel,
 
     inout data_bus_serial
 );
@@ -18,11 +25,27 @@ wire addr_valid;
 
 // address range checking
 addr_in_range #(
-    mem_offset = mem_offset, 
-    mem_size = mem_size
+    MEM_OFFSET = MEM_OFFSET, 
+    MEM_SIZE = MEM_SIZE
 )(
     .addr_in(addr_in),
     .in_range(addr_valid)
-)
+);
+
+// tristate buffers
+
+
+// main execution
+always @(posedge clk, negedge rstn) begin
+    if (rstn == 1'b0) begin
+        //reset logic
+        ready               <= 1'b0;
+        done                <= 1'b0;
+        data_out_parellel   <= (DATA_WIDTH{1'b0});
+    
+    end else begin
+
+    end
+end
 
 endmodule 
