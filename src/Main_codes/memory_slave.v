1f5d8267bc869620da1b77f0b47f7624f0cef390
Jester-2-6 <cwimalasuriya@gmail.com>
2019-06-01 23:57:29 +0530

    memory slave added

---
+++ src/Main_codes/memory_slave.v
@@ -0,0 +1,52 @@
+/*
+Module name  : memory_slave.v
+Author 	     : C.Wimalasuriya
+Date Modified: 01/06/2019
+Organization : ABruTECH
+Description  : Slave module with memory of the bus
+*/
+module memory_slave#(
+    parameter MEM_OFFSET = 0,
+    parameter MEM_SIZE = 2048,
+    parameter ADDRESS_WIDTH = 12
+)(
+    input [ADDRESS_WIDTH - 1:0] addr_in,
+    input clk, rstn, write_en,
+
+    output wire ready, done,
+
+    inout data_bus_serial
+);
+
+    wire [7:0]                  data_in_parellel;
+    wire [7:0]                  data_out_parellel;
+    wire [ADDRESS_WIDTH -1:0]   addr_out;
+    wire                        write_en_int;
+
+    slave #(
+        .MEM_OFFSET(MEM_OFFSET),
+        .MEM_SIZE(MEM_SIZE),
+        .ADDRESS_WIDTH(ADDRESS_WIDTH),
+        .DATA_WIDTH(8)
+    )Slave_inst(
+        .addr_in(addr_in),
+        .write_en(write_en),
+        .data_in_parellel(data_in_parellel),
+        .clk(clk),
+        .ready(ready),
+        .done(done),
+        .rstn(rstn),
+        .data_out_parellel(data_out_parellel),
+        .addr_out(addr_out),
+        .data_bus_serial(data_bus_serial)
+    );
+
+    ram_2k(
+        .address(addr_out),
+        .clock(clk),
+        .data(data_out_parellel),
+        .wren(write_en_int),
+        .q(data_in_parellel)
+    );
+
+endmodule
--- src/Main_codes/slave.v
+++ src/Main_codes/slave.v
@@ -15,107 +15,123 @@ module slave #(
     input clk, rstn, write_en,
     input [DATA_WIDTH - 1:0] data_in_parellel,
 
-    output reg ready, done,
+    output reg ready, done, write_en_internal,
     output reg [DATA_WIDTH - 1:0] data_out_parellel,
     output wire [ADDRESS_WIDTH -1:0] addr_out,
 
     inout data_bus_serial
 );
 
-localparam IDLE         = 3'd0;
-localparam WRITE        = 3'd1;
-localparam FETCH        = 3'd2;
-localparam DATA_READY   = 3'd3;
-localparam SERIAL_TX    = 3'd4;
-localparam CLEANUP      = 3'd5;
+    localparam IDLE         = 3'd0;
+    localparam WRITE_FETCH  = 3'd1;
+    localparam WRITE_EXEC   = 3'd2;
+    localparam READ_FETCH   = 3'd3;
+    localparam DATA_READY   = 3'd4;
+    localparam SERIAL_TX    = 3'd5;
+    localparam CLEANUP      = 3'd6;
 
-localparam DATA_WIDTH_LOG = $clog2(DATA_WIDTH);
+    localparam DATA_WIDTH_LOG = $clog2(DATA_WIDTH);
 
-wire addr_valid;
-reg [2:0] state;
+    wire addr_valid;
+    reg [2:0] state;
 
-reg serial_out_enable                               = 1'b0;
-reg serial_out_buff                                 = 1'b0;
-reg parellel_out_valid;
+    reg serial_out_enable                              = 1'b0;
+    reg serial_out_buff                                = 1'b0;
 
-reg [DATA_WIDTH - 1:0] parellel_out_buff               = (DATA_WIDTH{1'b0});
-reg [DATA_WIDTH - 1:0] parellel_in_buff            = (DATA_WIDTH{1'b0});
+    reg [DATA_WIDTH - 1:0] parellel_out_buff           = {DATA_WIDTH{1'b0}};
+    reg [DATA_WIDTH - 1:0] parellel_in_buff            = {DATA_WIDTH{1'b0}};
 
-reg [DATA_WIDTH_LOG - 1:0] serial_data_counter  = 0;
+    reg [DATA_WIDTH_LOG - 1:0] serial_data_counter  = 0;
 
-// address range checking
-addr_in_range #(
-    MEM_OFFSET = MEM_OFFSET, 
-    MEM_SIZE = MEM_SIZE
-)(
-    .addr_in(addr_in),
-    .in_range(addr_valid)
-);
+    // address range checking
+    addr_in_range #(
+        .MEM_OFFSET(MEM_OFFSET), 
+        .MEM_SIZE(MEM_SIZE)
+    )
+    range_checker
+    (
+        .addr_in(addr_in),
+        .in_range(addr_valid)
+    );
 
-// tristate buffers
-assign data_bus_serial = serial_out_enable ? serial_out_buff : 1'bZ;
+    // tristate buffers
+    assign data_bus_serial = serial_out_enable ? serial_out_buff : 1'bZ;
 
-// assignments
-assign addr_out = addr_in & (ADDRESS_WIDTH{addr_valid});
+    // assignments
+    assign addr_out = addr_in & {ADDRESS_WIDTH{addr_valid}};
 
-// main execution
-always @(posedge clk, negedge rstn) begin
-    if (rstn == 1'b0) begin
-        //reset logic
-        ready                   <= 1'b0;
-        done                    <= 1'b0;
-        data_out_parellel       <= (DATA_WIDTH{1'b0});
-        serial_out_enable       <= 1'b0;
-        state                   <= IDLE;
-        parellel_out_buff       <= (DATA_WIDTH{1'b0});
-        parellel_in_buff        <= (DATA_WIDTH{1'b0});
-        serial_out_buff         <= 1'b0;
-        serial_data_counter     <= (DATA_WIDTH_LOG{1'b0});
+    // main execution
+    always @(posedge clk, negedge rstn) begin
+        if (rstn == 1'b0) begin
+            //reset logic
+            ready                   <= 1'b0;
+            done                    <= 1'b0;
+            data_out_parellel       <= {DATA_WIDTH{1'b0}};
+            serial_out_enable       <= 1'b0;
+            state                   <= IDLE;
+            parellel_out_buff       <= {DATA_WIDTH{1'b0}};
+            parellel_in_buff        <= {DATA_WIDTH{1'b0}};
+            serial_out_buff         <= 1'b0;
+            serial_data_counter     <= {DATA_WIDTH_LOG{1'b0}};
+            write_en_internal       <= 1'b0;
 
-    end else begin
-        case(state)
+        end else begin
+            case(state)
 
-            IDLE: begin
-                if (addr_valid & ~write_en) state <= FETCH;
-                else if addr_valid state <= WRITE;
-            end
-
-            WRITE: begin
-                if serial_data_counter < DATA_WIDTH begin
-                    parellel_out_buff <= parellel_out_buff & (data_bus_serial << serial_data_counter);
-                    serial_data_counter <= serial_data_counter + 1;
-
-                end else begin
-                    state <= IDLE;
-                    parellel_out_valid <= 1'b1;
+                IDLE: begin
+                    if (addr_valid & write_en) state <= WRITE_FETCH;
+                    else if (addr_valid) state <= READ_FETCH;
                 end
-            end
 
-            FETCH: begin
-                parellel_in_buff <= data_in_parellel;
-                state <= DATA_READY;
-            end
+                WRITE_FETCH: begin
+                    if (serial_data_counter < DATA_WIDTH) begin
+                        parellel_out_buff   <= parellel_out_buff & (data_bus_serial << serial_data_counter);
+                        serial_data_counter <= serial_data_counter + 1;
 
-            DATA_READY: begin
-                if addr_valid state <= SERIAL_TX;
-            end
+                    end else begin
+                        state <= WRITE_EXEC;
+                    end
+                end
 
-            SERIAL_TX: begin
-                if serial_data_counter < DATA_WIDTH begin
-                    serial_out_buff         <= parellel_in_buff[serial_data_counter];
-                    serial_data_counter     <= serial_data_counter + 1;
+                WRITE_EXEC: begin
+                    write_en_internal   <= 1'b1;
+                    state               <= CLEANUP;  
+                end
 
-                end else state <= IDLE;
-            end
+                READ_FETCH: begin
+                    parellel_in_buff    <= data_in_parellel;
+                    state               <= DATA_READY;
+                end
 
-            CLEANUP: begin
-                parellel_out_buff       <= (DATA_WIDTH{1'b0});
-                parellel_in_buff        <= (DATA_WIDTH{1'b0});
-                serial_out_buff         <= 1'b0;
-                serial_data_counter     <= (DATA_WIDTH_LOG{1'b0});
-                state                   <= IDLE;
-            end
+                DATA_READY: begin
+                    ready                   <= 1'b1;
+                    if (addr_valid) state   <= SERIAL_TX;
+                end
+
+                SERIAL_TX: begin
+                    ready <= 1'b0;
+
+                    if (serial_data_counter < DATA_WIDTH) begin
+                        serial_out_buff         <= parellel_in_buff[serial_data_counter];
+                        serial_data_counter     <= serial_data_counter + 1;
+
+                    end else begin
+                        state <= IDLE;
+                        done <= 1'b1;
+                    end
+                end
+
+                CLEANUP: begin
+                    parellel_out_buff       <= {DATA_WIDTH{1'b0}};
+                    parellel_in_buff        <= {DATA_WIDTH{1'b0}};
+                    serial_out_buff         <= 1'b0;
+                    serial_data_counter     <= {DATA_WIDTH_LOG{1'b0}};
+                    done                    <= 1'b0;
+                    write_en_internal       <= 1'b0;
+                    state                   <= IDLE;
+                end
+            endcase
+        end
     end
-end
 
 endmodule 
