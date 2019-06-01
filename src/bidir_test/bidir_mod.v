5840479c32b657f5dfdb3388819810d4ae22db76
Jester-2-6 <cwimalasuriya@gmail.com>
2019-06-01 14:17:11 +0530

    bidir new design

--- src/bidir_test/bidir_mod.v
+++ src/bidir_test/bidir_mod.v
@@ -14,22 +14,34 @@ module bidir_mod(
     output [1:0] bus
 );
 
- bidir_unit mod1 (
-    .sel(sel),
+wire [3:0] sel_int;
+
+always @(sel):
+begin
+  case (sel)
+    2'b00: sel_int <= 4'b0000
+    2'b01: sel_int <= 4'b0010
+    2'b10: sel_int <= 4'b0100
+    2'b11: sel_int <= 4'b1000
+    default
+end
+
+bidir_unit mod1 (
+    .sel(sel_int[1]),
     .bus_wire(bus)
     );
 
 defparam mod1.my_val = 2'b01;
 
- bidir_unit mod2 (
-    .sel(sel),
+bidir_unit mod2 (
+    .sel(sel_int[2]),
     .bus_wire(bus)
     );
 
 defparam mod2.my_val = 2'b10;
 
- bidir_unit mod3 (
-    .sel(sel),
+bidir_unit mod3 (
+    .sel(sel_int[3]),
     .bus_wire(bus)
     );
 
