aefe502ef93271025997e346cd14a1fc2189c081
Rukshan Ravihansa Wickramasinghe <35403699+BlazeCode2@users.noreply.github.com>
2019-06-06 03:39:13 +0530

    Started top module

---
+++ src/Main_codes/bus_top_module.v
@@ -0,0 +1,33 @@
+/*
+Module name  : bus_top_module.v
+Author 	     : W.M.R.R.Wickramasinghe
+Date Modified: 06/06/2019
+Organization : ABruTECH
+Description  : Top module containig the arbiter,masters and slaves
+*/
+
+module bus_top_module(
+    rstn,
+    clk,
+    tx0,
+    rx0,
+    tx1,
+    rx1,
+    hex0,
+    hex1,
+    hex2,
+    hex3,
+    hex4,
+    requests,
+    utilization,
+    slave_busy,
+    mux_switch
+);
+
+// Parameters
+
+// Port declaration
+
+
+
+endmodule
