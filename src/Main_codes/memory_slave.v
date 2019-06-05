08fb6e78f9dceadab3ac9fd2b497bffc60ebfa8f
Jester-2-6 <cwimalasuriya@gmail.com>
2019-06-05 12:18:48 +0530

    memory slave updated

--- src/Main_codes/memory_slave.v
+++ src/Main_codes/memory_slave.v
@@ -5,48 +5,82 @@ Date Modified: 01/06/2019
 Organization : ABruTECH
 Description  : Slave module with memory of the bus
 */
-module memory_slave#(
+module memory_slave #(
     parameter MEM_OFFSET = 0,
     parameter MEM_SIZE = 2048,
     parameter ADDRESS_WIDTH = 12
 )(
-    input [ADDRESS_WIDTH - 1:0] addr_in,
-    input clk, rstn, write_en,
+    input clk, rstn,
 
-    output wire ready, done,
+    output wire [6:0] disp_out2, 
+    output wire [6:0] disp_out1, 
+    output wire [6:0] disp_out0,        
 
-    inout data_bus_serial
+    inout data_bus_serial, slave_busy
 );
+
+    localparam IDLE     = 1'd0;
+    localparam MEM_WRT  = 1'd1;
 
     wire [7:0]                  data_in_parellel;
     wire [7:0]                  data_out_parellel;
     wire [ADDRESS_WIDTH -1:0]   addr_out;
     wire                        write_en_int;
 
+    reg state = IDLE;
+    reg module_dv   = 1'b0;
+
     slave #(
-        .MEM_OFFSET(MEM_OFFSET),
-        .MEM_SIZE(MEM_SIZE),
         .ADDRESS_WIDTH(ADDRESS_WIDTH),
         .DATA_WIDTH(8)
     )Slave_inst(
-        .addr_in(addr_in),
-        .write_en(write_en),
-        .data_in_parellel(data_in_parellel),
         .clk(clk),
-        .ready(ready),
-        .done(done),
         .rstn(rstn),
+        .rd_wrt(rd_wrt),
+        .bus_util(bus_util),
+        .module_dv(module_dv),
+        .data_bus_serial(data_bus_serial),
+        .slave_busy(busy_wire),
+        .data_in_parellel(data_in_parellel),
+
+        .write_en_internal(write_en_internal),
         .data_out_parellel(data_out_parellel),
-        .addr_out(addr_out),
-        .data_bus_serial(data_bus_serial)
+        .addr_buff(addr_out)
+    );
+
+    bi2bcd display(
+        .din(data_in_parellel),
+        .dout2(disp_out2),
+        .dout1(disp_out1),
+        .dout0(disp_out0)
     );
 
     ram_2k ram_inst(
         .address(addr_out),
         .clock(clk),
         .data(data_out_parellel),
-        .wren(write_en_int),
+        .wren(write_en_internal),
         .q(data_in_parellel)
     );
+
+    always @(posedge clk, negedge rstn) begin
+        if (~rstn) begin
+            state <= IDLE;
+            module_dv <= 1'b0;
+        end else begin
+            case (state)
+                IDLE: begin
+                    module_dv <= 1'b0;
+                    if (write_en_internal) state <= MEM_WRT;
+                end
+
+                MEM_WRT: begin
+                    state       <= IDLE;
+                    module_dv   <= 1'b1;
+                end
+            endcase
+        end
+
+    end
 
 endmodule
